2892337d8349a5e4ccc510317a0d73d4a17b9615
fiorelln <mutiaradesyindah74@gmail.com>
2026-05-17 21:18:30 +0700

    adding readme

--- controllers/surat.go
+++ controllers/surat.go
@@ -3,44 +3,38 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/fiorelln/disposisi/config"
 	"github.com/fiorelln/disposisi/models"
-
 	"github.com/gin-gonic/gin"
 )
 
 func UploadSurat(c *gin.Context) {
 	file, err := c.FormFile("file")
-
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "File wajib diupload",
-		})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "File wajib diupload"})
 		return
 	}
 
 	if filepath.Ext(file.Filename) != ".pdf" {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "File harus PDF",
-		})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "File harus PDF"})
 		return
 	}
 
-	filename := fmt.Sprintf(
-		"%d_%s",
-		time.Now().Unix(),
-		file.Filename,
-	)
+	uploadPath := "uploads/surat"
+	if err := os.MkdirAll(uploadPath, os.ModePerm); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal membuat folder upload"})
+		return
+	}
 
-	path := "uploads/surat/" + filename
+	filename := fmt.Sprintf("%d_%s", time.Now().Unix(), file.Filename)
+	path := uploadPath + "/" + filename
 
 	if err := c.SaveUploadedFile(file, path); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "Gagal upload file",
-		})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal upload file"})
 		return
 	}
 
@@ -49,7 +43,10 @@ func UploadSurat(c *gin.Context) {
 		Status:    "dikirim",
 	}
 
-	config.DB.Create(&surat)
+	if err := config.DB.Create(&surat).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "DB insert gagal"})
+		return
+	}
 
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Upload surat berhasil",
--- models/disposisi.go
+++ models/disposisi.go
@@ -1,1 +1,11 @@
 package models
+
+import "gorm.io/gorm"
+
+type Disposisi struct {
+        gorm.Model
+        SuratID uint
+        Tujuan  string
+        Catatan string
+        Status  string 
+}
